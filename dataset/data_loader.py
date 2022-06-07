0b9688706fe6a875b926958ac482f35e7f4d669c
lxh21k <lxh1030402003@gmail.com>
2022-06-07 15:11:55 +0800

    Json parser

---
+++ dataset/data_loader.py
@@ -0,0 +1,60 @@
+from cgi import test
+from itertools import tee
+import os
+import random
+
+import nori2 as nori
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from reds_dataset import REDSDataset
+
+def worker_init_fn(worker_id, num_workers, rank, seed):
+    # Set the worker seed to num_workers * rank + worker_id + seed
+    worker_seed = num_workers * rank + worker_id + seed
+    random.seed(worker_seed)
+    np.random.seed(worker_seed)
+
+def fetch_dataloader(params):
+
+    if params.dataset_type == 'REDS':
+        train_ds = REDSDataset(params.train_gt_root, params.train_lq_root, params.train_meta_info_file, params.num_frame)
+        val_ds = REDSDataset(params.val_gt_root, params.val_lq_root, params.val_meta_info_file, params.num_frame)
+        test_ds = REDSDataset(params.test_gt_root, params.test_lq_root, params.test_meta_info_file, params.num_frame)
+
+    dataloaders = {}
+    train_dl = DataLoader(train_ds,
+                          batch_size=params.batch_size, 
+                          shuffle=False, 
+                          num_workers=params.num_workers,
+                          drop_last=True,
+                          pin_memory=params.cuda,
+                          worker_init_fn=worker_init_fn
+                )
+
+    dataloaders['train'] = train_dl
+
+    for split in ['val', 'test']:
+        if split in params.eval_type:
+            if split == 'val':
+                dl = DataLoader(val_ds,
+                                batch_size=params.eval_batch_size, # 1
+                                shuffle=False,
+                                num_workers=params.num_workers,
+                                pin_memory=params.cuda,
+                    )
+            if split == 'test':
+                dl = DataLoader(test_ds,
+                                batch_size=params.eval_batch_size, # 1
+                                shuffle=False,
+                                num_workers=params.num_workers,
+                                pin_memory=params.cuda,
+                    )
+            else:
+                raise ValueError("Unknow eval_type in params, should in [val, test]")
+            dataloaders[split] = dl
+        else:
+            dataloaders[split] = None
+
+    return dataloaders
---
+++ train.py
@@ -0,0 +1,64 @@
+import argparse
+from csv import writer
+import datetime
+import os
+
+import torch
+import torch.optim as optim
+
+from tqdm import tqdm
+
+import common.utils as utils
+import dataset.data_loader as data_loader
+from models.lr_scheduler import CosineAnnealingRestartLR
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_dir', help="Directory containing params.json")
+
+if __name__ == '__main__':
+    # Load the parameters from json file
+    args = parser.parse_args()
+    json_path = os.path.join(args.model_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = utils.Params(json_path)
+
+    # Update args into params
+    # vars(args) is a dict of all the arguments passed to the script
+    print(vars(args))   
+    params.update(vars(args))
+
+    # use GPU if available
+    params.cuda = torch.cuda.is_available()
+
+    # Set the random seed for reproducible experiments
+    torch.manual_seed(params.seed)
+    if params.cuda:
+        torch.cuda.manual_seed(params.seed)
+
+    dataloaders = data_loader.fetch_dataloader(params)
+
+    exit()
+
+    if params.cuda:
+        model = net.fetch_net(params).cuda()
+        model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+    else:
+        model = net.fetch_net(params)
+
+    optimizer = optim.Adam(model.parameters(), lr=params.lr, betas=(0.9, 0.999))
+    scheduler = CosineAnnealingRestartLR(optimizer=optimizer,
+                                         periods=params.scheduler.periods,
+                                         restart_weights=params.scheduler.restart_weights,
+                                         eta_min=params.scheduler.eta_min
+                                        )
+
+    manager = Manager(model=model, 
+                      optimizer=optimizer, 
+                      scheduler=scheduler, 
+                      params=params, 
+                      dataloaders=dataloaders,
+                      # writer=writer
+                      # logger=logger
+                      )
+
+    train_and_evaluate(model, manager)
